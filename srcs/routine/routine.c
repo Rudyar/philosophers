7dc623baa092b5ee769b30480d7cae8d9b4a67da
arudy <arthur.rudy.code@gmail.com>
2022-03-15 17:08:33 +0100

    gros boulot

--- srcs/routine/routine.c
+++ srcs/routine/routine.c
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 14:05:57 by arudy             #+#    #+#             */
-/*   Updated: 2022/03/15 16:49:23 by arudy            ###   ########.fr       */
+/*   Updated: 2022/03/15 17:06:26 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,6 @@ void	*start_routine(void *philo)
 	while (p->data->stop == 0)
 	{
 		eat_routine(p);
-		// if (p->count_eat == p->data->nb_must_eat)
-		// 	break ;
 		sleep_think_routine(p);
 	}
 	return (NULL);
@@ -74,15 +72,14 @@ int	check_death(t_data *data)
 		j = 0;
 		while (i < data->nb_philo)
 		{
+			pthread_mutex_lock(&data->philo[i].last_eat_mutex);
 			if (get_time() - data->philo[i].last_eat - data->start_time > data->time_to_die)
 			{
-			pthread_mutex_lock(&data->write_mutex);
-			pthread_mutex_lock(&data->stop_mutex);
-			print_status("died\n", &data->philo[i]);
-			data->stop = 1;
-			pthread_mutex_unlock(&data->stop_mutex);
-			return (1);
-			pthread_mutex_unlock(&data->write_mutex);
+				pthread_mutex_lock(&data->write_mutex);
+				print_status("died\n", &data->philo[i]);
+				return (1);
+				pthread_mutex_unlock(&data->write_mutex);
+				pthread_mutex_unlock(&data->philo[i].last_eat_mutex);
 			}
 			i++;
 		}
@@ -94,12 +91,7 @@ int	check_death(t_data *data)
 			i++;
 		}
 		if (j == data->nb_must_eat)
-		{
-			pthread_mutex_lock(&data->stop_mutex);
-			data->stop = 1;
-			pthread_mutex_unlock(&data->stop_mutex);
 			return (1);
-		}
 	}
 	return (0);
 }
@@ -120,7 +112,8 @@ void	start_philo(t_data *data)
 	{
 		if (check_death(data) == 1)
 		{
-			break ;
+			data->stop = 1;
+			return ;
 		}
 	}
 }
